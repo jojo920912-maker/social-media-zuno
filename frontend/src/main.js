e8397027ff924d29d44d336a440932aac7e8f8b5
Rzhe <jojo920912@gmail.com>
2026-05-25 03:43:31 +0800

    fix: set axios baseURL to Render backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,12 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import naive from 'naive-ui'
+import axios from 'axios'
 
 import App from '@/App.vue'
 import router from '@/router'
+
+axios.defaults.baseURL = 'https://social-media-53bm.onrender.com'
 
 const app = createApp(App)
 
